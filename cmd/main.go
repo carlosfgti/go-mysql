8a160e1fc135c559d28c0ae24d2f0d8259a7e40b
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-05 09:49:57 -0300

    feat: select rows with mysql

--- cmd/main.go
+++ cmd/main.go
@@ -20,4 +20,23 @@ func main() {
 		panic(err)
 	}
 	fmt.Print("Successfully connected!")
+
+	selectRows(db)
+}
+
+func selectRows(db *sql.DB) {
+	rows, err := db.Query("SELECT id, name FROM users")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(id, name)
+	}
 }
