af48dbd7cfe0a961a7ee74da6e0129113d9081d2
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-05 09:43:44 -0300

    feat: connect mysql successfully

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
-	print("hello")
+	dsn := "username:userpass@tcp(127.0.0.1:3306)/goproject"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Successfully connected!")
 }
